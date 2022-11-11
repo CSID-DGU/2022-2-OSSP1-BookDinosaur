2a89909f7b0b7652cc11d9cb872984102c226b97
Daeyong Jeong <daeyong.jeong.18@gmail.com>
2022-11-11 13:45:57 +0900

    Format JavaScript files

--- client/src/component/Header.js
+++ client/src/component/Header.js
@@ -8,66 +8,66 @@ import styled from 'styled-components'; //CSS-IN_JS
 import './Header.css'
 
 const Spacing = styled.div`
-    width: 5%;
-    height: 30px;
+  width: 5%;
+  height: 30px;
 `;
 
 //background-color: #fcc879; #fff6b0; #ffdeab;
 const MyInfo = styled.div`
-    width: 30%;
-    height: 40px;
-    background-color: #ffdeab;
-    border-radius: 5px;
-    margin: 10px;
-    box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.16);
-    font-size: 1rem;
+  width: 30%;
+  height: 40px;
+  background-color: #ffdeab;
+  border-radius: 5px;
+  margin: 10px;
+  box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.16);
+  font-size: 1rem;
 `;
 
 const Header = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [bookReportCount, setBookReportCount] = useState(0)
-    // eslint-disable-next-line
-    const [cookies, removeCookie] = useCookies(['user']);
+  const [bookReportCount, setBookReportCount] = useState(0)
+  // eslint-disable-next-line
+  const [cookies, removeCookie] = useCookies(['user']);
 
-    axios.get('/api/db/users/bookreports/' + cookies?.user?.userId).then((res) => {
-        setBookReportCount(res.data.data?.length || 0);
-    }).catch((err) => {
-        console.log(err);
-    });
+  axios.get('/api/db/users/bookreports/' + cookies?.user?.userId).then((res) => {
+    setBookReportCount(res.data.data?.length || 0);
+  }).catch((err) => {
+    console.log(err);
+  });
 
-    const path = window.location.pathname;
-    if (path === '/' || path==='/SignUp') return null; /*로그인, 회원가입 페이지에서 Nav 숨기기 */
+  const path = window.location.pathname;
+  if (path === '/' || path === '/SignUp') return null; /*로그인, 회원가입 페이지에서 Nav 숨기기 */
 
-    return (
-        <div className="title-area">
-            <ImportContactsIcon fontSize='large'/>
-            <div onClick={() => navigate('/Main')}>
-                <h2>Read<br/>Lead</h2>
-            </div>
-            <Spacing />
-            <div>
-                <MultipleBookSearch />
-            </div>
-            <Spacing />
-            <MyInfo>
-                <div>{cookies?.user?.nickName}님 &nbsp;| &nbsp;내가 쓴 독후감 {bookReportCount}권 &nbsp;|</div>
-                <div onClick={() => {
-                    axios.get('/api/db/users/logout')
-                        .then((res) => {
-                            return res.data;
-                        })
-                        .then((data) => {
-                            alert("로그아웃 되었습니다");
-                            removeCookie('user');
-                            navigate('/');
-                        }).catch((err) => {
-                            alert('로그아웃에 실패했습니다.')
-                        })
-                }}>&nbsp;&nbsp;로그아웃</div>
-            </MyInfo>
-        </div>
-    )
+  return (
+    <div className="title-area">
+      <ImportContactsIcon fontSize='large' />
+      <div onClick={() => navigate('/Main')}>
+        <h2>Read<br />Lead</h2>
+      </div>
+      <Spacing />
+      <div>
+        <MultipleBookSearch />
+      </div>
+      <Spacing />
+      <MyInfo>
+        <div>{cookies?.user?.nickName}님 &nbsp;| &nbsp;내가 쓴 독후감 {bookReportCount}권 &nbsp;|</div>
+        <div onClick={() => {
+          axios.get('/api/db/users/logout')
+            .then((res) => {
+              return res.data;
+            })
+            .then((data) => {
+              alert("로그아웃 되었습니다");
+              removeCookie('user');
+              navigate('/');
+            }).catch((err) => {
+              alert('로그아웃에 실패했습니다.')
+            })
+        }}>&nbsp;&nbsp;로그아웃</div>
+      </MyInfo>
+    </div>
+  )
 };
 
 export default Header; 
--- client/src/component/ShortReport.js
+++ client/src/component/ShortReport.js
@@ -3,27 +3,26 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import './ShortReport.css'
 
-
 const Main = (props) => {
     const [reportList, setReportList] = useState([]);
     const navigate = useNavigate();
 
     useEffect(() => {
         axios.get('/api/db/books/bookreports/' + props.isbn)
-        .then((res) => {
-            setReportList(res.data.data || [])
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+            .then((res) => {
+                setReportList(res.data.data || [])
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     }, [props.isbn]);
 
     return (
         <div className="shortReport-area" >
             {reportList.length ? reportList.map((data, index) => {
                 return (
-                    <div className="report-box" key={index} onClick = {() => {
-                        navigate('/ViewReportPage', {state: {isbn: props.isbn, userid: data.userid}});
+                    <div className="report-box" key={index} onClick={() => {
+                        navigate('/ViewReportPage', { state: { isbn: props.isbn, userid: data.userid } });
                     }}>
                         <div className="title">
                             <p className="bookTitle">{data.title}</p>
@@ -42,4 +41,4 @@ const Main = (props) => {
     )
 };
 
-export default Main; 
+export default Main;
