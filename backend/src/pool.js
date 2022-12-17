a34f01ca88c01028f00c0fc61ead56971c9ac8e4
Daeyong Jeong <daeyong.jeong.18@gmail.com>
2022-12-17 14:37:23 +0900

    Update backend Dockerfile

--- backend/src/pool.js
+++ backend/src/pool.js
@@ -5,10 +5,10 @@ require("dotenv").config({ path: "../.env" })
 const mariadb = require("mariadb")
 
 const pool = mariadb.createPool({
-    host: process.env.DB_HOST,
+    host: process.env.MARIADB_ROOT_HOST,
     port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PW,
+    user: process.env.MARIADB_USER,
+    password: process.env.MARIADB_PASSWORD,
     database: process.env.DB_NAME,
 })
 
